b039d1f63fda64a6b285b76af30544432fb9d1a0
Karishma8499896 <karishmakareena2002@gmail.com>
2024-09-12 12:39:00 +0530

    Add files via upload

--- factorial.java
+++ factorial.java
@@ -1,10 +1,16 @@
-class FactorialExample{  
- public static void main(String args[]){  
-  int i,fact=1;  
-  int number=5;//It is the number to calculate factorial    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
- }  
-}  
+public class Factorial {
+
+    // Recursive method to calculate factorial
+    public static long factorialRecursive(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorialRecursive(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5; // You can change this number to test other values
+        System.out.println("Factorial of " + number + " (recursive) is: " + factorialRecursive(number));
+    }
+}
